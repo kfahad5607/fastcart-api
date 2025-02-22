aa56f6047b1d3f54137a6fd3383f176bcb4ab42c
Fahad Khan <kfahad5607@gmail.com>
2025-02-22 16:49:35 +0530

    Basic API setup

---
+++ api/v1/routes/products.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from utils.logger import logger
+
+router = APIRouter()
+
+@router.get("/")
+async def get_products():
+    try:
+        return {
+            'current_page': 1,
+            'page_size': 10,
+            'total_records': 100,
+            'total_pages': 10,
+            'data': []
+        }
+    except Exception as e:
+        logger.error(f"Exception in get_products ==> {e}")
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from api.v1.routes import products
 
 app = FastAPI(debug=True)
+
+# Include routers
+app.include_router(
+    products.router, 
+    prefix="/api/v1/products", 
+    tags=["products"]
+)
+app.include_router(
+    products.router, 
+    prefix="/api/v1/orders", 
+    tags=["orders"]
+)
 
 @app.get("/health")
 async def health_check():
